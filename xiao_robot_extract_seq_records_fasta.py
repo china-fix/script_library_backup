e4703841e3bc932ffb566c0f2aec0468977998b7
china-fix <china-fixing@hotmail.com>
2019-03-27 21:56:49 +0100

    Add files via upload

---
+++ xiao_robot_extract_seq_records_fasta.py
@@ -0,0 +1,66 @@
+import sys
+import argparse
+from Bio import Seq
+from Bio import SeqIO
+from Bio import SeqRecord
+
+"""def parse_args():
+    parser=argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '--query', required=True, type=str, metavar='FILENAME',
+        help="Query genome filename"
+    )
+    parser.add_argument(
+        '--db', required=True, type=str, metavar='FILENAME',
+        help="CDS database filename"
+    )
+    parser.add_argument(
+        '--ident', type=float, default=0.95, metavar='FLOAT',
+        help="Identity threshold for saving hsps [0.95]"
+    )
+    parser.add_argument(
+        '--evalue', type=float, default=1e-10, metavar='FLOAT',
+        help="Expectation value threshold for saving hsps [1e-10]"
+    )
+    parser.add_argument(
+        '--output', type=argparse.FileType('w'), default=sys.stdout, metavar='FILENAME',
+        help="Output filename"
+    )
+    return parser.parse_args()"""
+
+def parse_args():
+    parser=argparse.ArgumentParser(description="Welcome to use Xiao_Fei_Robot")
+    parser.add_argument('--pangenome', required=True, type=str, metavar='FILENAME', help="roary output pangenome file")
+    parser.add_argument('--extract', required=True, type=str, metavar='FILENAME', help="gene names you want to extract, each line one name")
+    parser.add_argument('--output', required=True, type=str, metavar='FILENAME', help="Output filename")
+    return parser.parse_args()
+
+
+
+# triming the original pangenome fasta file
+def main():
+    args=parse_args()
+    seq_records=SeqIO.parse(args.pangenome,"fasta")
+    new_seq_records=[]
+    for seq_record in seq_records:
+        seq_record.id=seq_record.description.split()[1]
+        new_seq_records.append(seq_record)
+    SeqIO.write(new_seq_records,"temp_xiao_fei_robot", "fasta")
+
+# get the dictionary of trimmed pangenome fasta
+    seq_records_dict=SeqIO.index("temp_xiao_fei_robot","fasta")
+    print(len(seq_records_dict))
+    print(list(seq_records_dict.keys()))
+
+# extract the seq_records according the key_list
+    #key_list=["sodC_1","group_3250"]
+    with open(args.extract,'r') as f:
+        key_list=[line.rstrip('\n') for line in f]
+    extract_seq_records=[]
+    for key in key_list:
+        extract_seq_records.append(seq_records_dict[key])
+    SeqIO.write(extract_seq_records,args.output,"fasta")    
+
+
+if __name__ == '__main__':
+    sys.exit(main())
